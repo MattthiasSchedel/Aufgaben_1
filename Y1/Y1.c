0a15476e7735293c50fc26719a5d0857f18ee0a8
Matthias <Matthias@DESKTOP-U6G89GE>
2018-11-03 18:45:05 +0100

    y

--- Y1/Y1.c
+++ Y1/Y1.c
@@ -8,19 +8,12 @@ init_board(int X, int Y)
 {
 	for (int i = 0; i < (X*Y); i++)
 	{
-		if ((i < X - 1) || (i % X == 0) || (i > (X*Y) - X))
+		if ((i < X - 1) || (i % X == 0) || (i > (X*Y) - X)|| (i % X == X - 1))
 		{
-			printf("#");
-			Board[i+1] = 1;
-		}
-		else if (i % X == X - 1)
-		{
-			printf("#\n");
 			Board[i+1] = 1;
 		}
 		else 
 		{
-			printf(" ");
 			Board[i+1] = 0;
 		}
 	}
@@ -29,10 +22,14 @@ init_board(int X, int Y)
 void Blocks(int X, int Y)
 {
 	int Rand = rand(), r = (Rand % 4) + 3, E=Rand%(X*Y);
+	if (E > (X*Y) - (r * X))
+	{
+		E = E - r * X;
+	}
 	for (int g = 0; g < r; g++)
 	{
-		E = E + g * (Y - r);
-		for (int i = 0; i < r; i++)
+		E = E + X;
+		for (int i = 1; i <= r; i++)
 		{
 			Board[E + i] = Board[E + i] | 1;
 		}
@@ -53,12 +50,34 @@ void main()
 		{
 			Blocks(X, Y);
 		}
-		printf("\n");
+		/*printf("\n");
 		for (int i = 1; i <= (X*Y); i++)
 		{
 			printf("%d", Board[i]);
-		}
+		}*/
+		printer(X, Y);
+
 		printf("another one?(y)");
 		c = _getwch();
 	} while (c == 'y');
+}
+
+int printer(int X,int Y)
+{
+	for (int i = 0; i < X*Y; i++)
+	{
+		if ((Board[i + 1] == 1)&&(i%X!=X-1))
+		{
+			printf("#");
+		}
+		else if (Board[i + 1] == 0)
+		{
+			printf(" ");
+		}
+		else 
+		{
+			printf("#\n");
+		}
+		
+	}
 }
